0a5d86e68f0d1cbc4ae67d0071e8507afa8bda90
Resti <fitriaresti8@gmail.com>
2026-09-15 22:42:38 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Proyek Web Pertama</title>
+</head>
+<body>
+
+    <h1>Selamat Datang di Website Saya!</h1>
+    
+    <?php
+        $nama = "Resti";
+        $tanggal = date("d F Y");
+        
+        echo "<p>Halo <b>$nama</b>, hari ini tanggal <b>$tanggal</b>.</p>";
+        echo "<p>Kodingan PHP kamu berhasil berjalan via MAMP!</p>";
+    ?>
+
+</body>
+</html>
